098ebbe072e5f0280d965bce48fc92fb8b9e41ac
nerocui <nerocui@outlook.com>
2024-01-05 08:05:54 -0800

    successfully switched from monaco to codemirror
    everything works!!!

--- DevChat/DevChat.Client/Components/Conversation.razor.js
+++ DevChat/DevChat.Client/Components/Conversation.razor.js
@@ -8,3 +8,92 @@
         });
     }
 }
+
+let editorInstance = {
+    initialized: false,
+    htmlEditor: undefined,
+    jsEditor: undefined,
+    cssEditor: undefined,
+};
+
+export const initializeEditor = () => {
+    if (editorInstance.initialized) {
+        return;
+    }
+    const htmlTextArea = document.getElementById("code-mirror-editor-html");
+    const jsTextArea = document.getElementById("code-mirror-editor-js");
+    const cssTextArea = document.getElementById("code-mirror-editor-css");
+
+    editorInstance.htmlEditor = CodeMirror.fromTextArea(htmlTextArea, {
+        lineNumbers: true,
+        mode: { name: "xml", htmlMode: true },
+    });
+    editorInstance.htmlEditor.on("keydown", (cm, e) => {
+        DotNet.invokeMethodAsync("DevChat.Client", "IndicateTyping");
+    });
+    editorInstance.htmlEditor.on("keyup", function (cm, e) {
+        // Check if the key combination is Ctrl-Enter
+        if (e.ctrlKey && e.keyCode == 13) {
+            // Prevent the default behavior
+            e.preventDefault();
+            // Call a C# method named SayHello from the MyAssembly assembly
+            DotNet.invokeMethodAsync("DevChat.Client", "OnSendMessage");
+        }
+    });
+
+
+    editorInstance.jsEditor = CodeMirror.fromTextArea(jsTextArea, {
+        lineNumbers: true,
+        mode: "javascript"
+    });
+    editorInstance.jsEditor.on("keydown", (cm, e) => {
+        DotNet.invokeMethodAsync("DevChat.Client", "IndicateTyping");
+    });
+
+    editorInstance.cssEditor = CodeMirror.fromTextArea(cssTextArea, {
+        lineNumbers: true,
+        mode: "css"
+    });
+    editorInstance.cssEditor.on("keydown", (cm, e) => {
+        DotNet.invokeMethodAsync("DevChat.Client", "IndicateTyping");
+    });
+
+    editorInstance.initialized = true;
+}
+
+export const getHtmlString = () => {
+    if (!editorInstance.initialized) {
+        return "";
+    }
+
+    return editorInstance.htmlEditor.getValue();
+}
+
+export const resetHtmlString = () => {
+    editorInstance.htmlEditor?.setValue("");
+}
+
+export const getJsString = () => {
+    if (!editorInstance.initialized) {
+        return "";
+    }
+
+    return editorInstance.jsEditor.getValue();
+}
+
+export const resetJsString = () => {
+    editorInstance.jsEditor?.setValue("");
+}
+
+export const getCssString = () => {
+    if (!editorInstance.initialized) {
+        return "";
+    }
+
+    return editorInstance.cssEditor.getValue();
+}
+
+export const resetCssString = () => {
+    editorInstance.cssEditor?.setValue("");
+}
+
---
+++ DevChat/DevChat.Client/Services/CodeMirrorService.cs
@@ -0,0 +1,46 @@
+﻿using DevChat.Client.Components;
+using Microsoft.JSInterop;
+
+namespace DevChat.Client.Services;
+
+public class CodeMirrorService
+{
+    private delegate void SendMessage();
+    private delegate void OnKeyDown();
+
+    private static SendMessage m_SendMessage = null;
+    private static OnKeyDown m_OnKeyDown = null;
+
+    public static void Register(Conversation conversation)
+    {
+        m_SendMessage += conversation.OnSendKeyEvent;
+        m_OnKeyDown += conversation.OnKeyDown;
+    }
+
+    public static void Unregister(Conversation conversation)
+    {
+        if (m_SendMessage != null)
+        {
+            m_SendMessage -= conversation.OnSendKeyEvent;
+            m_OnKeyDown -= conversation.OnKeyDown;
+        }
+    }
+
+    [JSInvokable]
+    public static void OnSendMessage()
+    {
+        if (m_SendMessage != null)
+        {
+            m_SendMessage();
+        }
+    }
+
+    [JSInvokable]
+    public static void IndicateTyping()
+    {
+        if (m_OnKeyDown != null)
+        {
+            m_OnKeyDown();
+        }
+    }
+}
