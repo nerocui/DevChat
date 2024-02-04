2c5cdbf2b1a566a12bfb97d149725dfbe76be733
nerocui <nerocui@outlook.com>
2024-02-04 05:14:56 -0800

    polish

--- DevChat/DevChat.Share/Dtos/UserDtoForViewing.cs
+++ DevChat/DevChat.Share/Dtos/UserDtoForViewing.cs
@@ -5,4 +5,6 @@ public class UserDtoForViewing
     public string Id { get; set; }
     public string Email { get; set; }
     public string AvatarUrl { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
 }
---
+++ DevChat/DevChat/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using DevChat.Data;
+using DevChat.Share.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevChat.Controllers;
+
+[Route("user")]
+[Authorize]
+public class UserController : ControllerBase
+{
+    private UserManager<ApplicationUser> _userManager;
+
+    public UserController(UserManager<ApplicationUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    [HttpGet("GetAuthenticatedUser")]
+    public async Task<IActionResult> GetAuthenticatedUser()
+    {
+        var user = await _userManager.GetUserAsync(User);
+        return Ok(new UserDtoForViewing()
+        {
+            Id = user.Id,
+            Email = user.Email,
+            AvatarUrl = user.AvatarUrl,
+            FirstName = user.FirstName,
+            LastName = user.LastName,
+        });
+    }
+}
